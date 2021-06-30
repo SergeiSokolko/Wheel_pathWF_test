affa78a95c8fbe3a8ed98c953acff9d296946a70
SergeiSokolko <86717075+SergeiSokolko@users.noreply.github.com>
2021-06-30 15:33:29 +0300

    Add files via upload

---
+++ COURSE_PROJECT/Form1.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace COURSE_PROJECT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Radius = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Start_Stop = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.Reset = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Radius
+            // 
+            this.Radius.Location = new System.Drawing.Point(12, 31);
+            this.Radius.MaxLength = 4;
+            this.Radius.Name = "Radius";
+            this.Radius.Size = new System.Drawing.Size(69, 20);
+            this.Radius.TabIndex = 0;
+            this.Radius.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.Radius.TextChanged += new System.EventHandler(this.Radius_TextChanged);
+            this.Radius.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Radius_KeyPress);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(5, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(80, 13);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "Диаметр круга:";
+            // 
+            // Start_Stop
+            // 
+            this.Start_Stop.Location = new System.Drawing.Point(12, 57);
+            this.Start_Stop.Name = "Start_Stop";
+            this.Start_Stop.Size = new System.Drawing.Size(69, 23);
+            this.Start_Stop.TabIndex = 2;
+            this.Start_Stop.Text = "Старт";
+            this.Start_Stop.UseVisualStyleBackColor = true;
+            this.Start_Stop.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Start_Stop_MouseClick);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(112, 35);
+            this.trackBar1.Maximum = 4;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.TabIndex = 3;
+            this.trackBar1.ValueChanged += new System.EventHandler(this.trackBar1_ValueChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Location = new System.Drawing.Point(112, 13);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(104, 13);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "Угловая скорость";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Reset
+            // 
+            this.Reset.Location = new System.Drawing.Point(12, 86);
+            this.Reset.Name = "Reset";
+            this.Reset.Size = new System.Drawing.Size(69, 23);
+            this.Reset.TabIndex = 5;
+            this.Reset.Text = "Сброс";
+            this.Reset.UseVisualStyleBackColor = true;
+            this.Reset.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Reset_MouseClick);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(269, 31);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(45, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(269, 59);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(45, 20);
+            this.textBox4.TabIndex = 7;
+            // 
+            // textBox5
+            // 
+            this.textBox5.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox5.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox5.Location = new System.Drawing.Point(239, 33);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(28, 13);
+            this.textBox5.TabIndex = 8;
+            this.textBox5.Text = "X:";
+            this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox6
+            // 
+            this.textBox6.BackColor = System.Drawing.SystemColors.Control;
+            this.textBox6.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox6.Location = new System.Drawing.Point(239, 62);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(28, 13);
+            this.textBox6.TabIndex = 9;
+            this.textBox6.Text = "Y:";
+            this.textBox6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Красный",
+            "Синий",
+            "Зеленый",
+            "Оранжевый",
+            "Индиго"});
+            this.comboBox1.Location = new System.Drawing.Point(353, 31);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(81, 21);
+            this.comboBox1.TabIndex = 10;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox7
+            // 
+            this.textBox7.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox7.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox7.Location = new System.Drawing.Point(353, 12);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(81, 13);
+            this.textBox7.TabIndex = 11;
+            this.textBox7.Text = "Цвет линии";
+            this.textBox7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.Reset);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.Start_Stop);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Radius);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Radius;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Start_Stop;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button Reset;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox7;
+    }
+}
+
---
+++ COURSE_PROJECT/Form1.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace COURSE_PROJECT
+{
+    public partial class Form1 : Form
+    {
+#region Variables
+        int velocity;
+        int diameter;
+        int coordY;
+        float rotationSpeed = 0.01f;
+        long totalMemory = GC.GetTotalMemory(false);
+        Color colors; Brush brushes;
+        List<Point> points = new List<Point>();
+#endregion
+
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+
+            Wheel wheel = new Wheel(ClientSize, e.Graphics, diameter, velocity, rotationSpeed, colors);
+
+            Graphics gra = e.Graphics;
+            System.Diagnostics.Process.GetCurrentProcess().PriorityClass = System.Diagnostics.ProcessPriorityClass.RealTime; //приоритет выполняемому процессу
+
+            int x = (int)(diameter / 2 * Math.Sin(-velocity * rotationSpeed) + (diameter / 2));
+            int y = (int)(diameter / 2 * Math.Cos(-velocity * rotationSpeed) + (diameter / 2));
+
+            points.Add(new Point(velocity + x + 5, this.Height - diameter + y - 43));
+
+            Track track = new Track(e.Graphics, points, colors, brushes);
+            coordY = diameter - y-1;
+
+            textBox3.Text = (velocity + x + 5).ToString();
+            textBox4.Text = coordY.ToString();
+        }
+
+        private void Radius_TextChanged(object sender, EventArgs e)
+        {
+
+            try
+            {
+                Start_Stop.Enabled = true;
+                var diam = Convert.ToInt32(Radius.Text);    //диаметр
+                
+                if (diam >= this.Width || diam >= this.Height)
+                {
+                    MessageBox.Show("Введённое значение диаметра превышает границы области");
+                    Start_Stop.Enabled = false;
+                }
+                diameter = diam;
+            }
+            catch (Exception ex)
+            {
+                    //MessageBox.Show("Ошибка! " + ex);
+            }
+        }
+
+        private void Radius_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!Char.IsDigit(e.KeyChar) && e.KeyChar != 8)     //8 - код клавиши backspace
+                e.Handled = true;
+        }
+
+        private void Start_Stop_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (Start_Stop.Text == "Старт" && Radius.Text != "")
+            {
+                Start_Stop.Text = ("Стоп");
+                if (velocity < 1)
+                    points.Clear();
+                Radius.Enabled = false;
+                timer1.Start();
+            }
+            else
+            {
+                Start_Stop.Text = ("Старт");
+                Radius.Enabled = true;
+                timer1.Stop();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (velocity >= (Width - diameter - 30)) //заканчивает движение круга, когда он доходит до правого края поля
+            {
+                timer1.Stop();
+                Start_Stop.Text = ("Старт");
+                Start_Stop.Enabled = false;
+            }
+
+            velocity += 1; // velocity отвечает за координату Х. Интервал задается в миллисекундах, т.е. 1000 = 1 секунда
+            this.Invalidate(); //отрисовка круга с каждым тиком таймера
+
+        }
+
+        private void Reset_MouseClick(object sender, MouseEventArgs e)
+        {
+            velocity = 0;
+            points.Clear();
+            Start_Stop.Enabled = true;
+            Radius.Enabled = true;
+            if (diameter >= this.Width || diameter >= this.Height)
+            {
+                Radius.Text = "";
+                points.Clear();
+            }
+            GC.Collect();
+            //GC.WaitForPendingFinalizers();
+            this.Invalidate();
+        }
+
+        private void trackBar1_ValueChanged(object sender, EventArgs e)
+        {
+            switch (trackBar1.Value)
+            {
+                case 0:
+                    rotationSpeed = 0.01f; break;
+                case 1:
+                    rotationSpeed = 0.05f; break;
+                case 2:
+                    rotationSpeed = 0.1f; break;
+                case 3:
+                    rotationSpeed = 0.15f; break;
+                case 4:
+                    rotationSpeed = 0.5f; break;
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox1.SelectedIndex)
+            {
+                case 0:
+                    { colors = Color.Red; brushes = Brushes.Red;  this.Invalidate(); break; }
+                case 1:
+                    { colors = Color.Blue; brushes = Brushes.Blue; this.Invalidate(); break; }
+                case 2:
+                    { colors = Color.Green; brushes = Brushes.Green; this.Invalidate(); break; }
+                case 3:
+                    { colors = Color.Orange; brushes = Brushes.Orange; this.Invalidate(); break; }
+                case 4:
+                    { colors = Color.Indigo; brushes = Brushes.Indigo; this.Invalidate(); break; }
+            }
+        }
+    }
+}
